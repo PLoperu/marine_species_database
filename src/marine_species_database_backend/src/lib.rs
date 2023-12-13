77d110962883e79f8b70df2f1813b2908ed9e5ee
Muhindo Galien <74584848+Muhindo-Galien@users.noreply.github.com>
2023-12-13 15:47:58 +0300

    Updated canister

--- src/marine_species_database_backend/src/lib.rs
+++ src/marine_species_database_backend/src/lib.rs
@@ -7,36 +7,33 @@ use ic_stable_structures::{BoundedStorable, Cell, DefaultMemoryImpl, StableBTree
 use validator::Validate;
 use std::{borrow::Cow, cell::RefCell};
 
-
 type Memory = VirtualMemory<DefaultMemoryImpl>;
 type IdCell = Cell<u64, Memory>;
 
 #[derive(candid::CandidType, Clone, Serialize, Deserialize)]
 struct Taxonomy {
-    id:u64,
-    kingdom:String,
-    phylum:String,
-    class:String,
-    order:String,
-    family:String,
-    genus:String,
-    species:String,
-    created_at:u64,
-    updated_at:Option<u64>
+    id: u64,
+    kingdom: String,
+    phylum: String,
+    class: String,
+    order: String,
+    family: String,
+    genus: String,
+    species: String,
+    created_at: u64,
+    updated_at: Option<u64>,
 }
 
 #[derive(candid::CandidType, Clone, Serialize, Deserialize, Default)]
 struct MarineSpecie {
     id: u64,
     name: String,
-    habitat:String,
-    taxonomy_id: u64,//Reference to Taxonomy by Id
-    conservation_status:String,  //Can be eg:Extinct,CriticallyEndagered,, Endagered, vulnerable,LeastConcern
-    created_at:u64,
-    updated_at:Option<u64>
+    habitat: String,
+    taxonomy_id: u64, // Reference to Taxonomy by Id
+    conservation_status: String, // Can be eg: Extinct, CriticallyEndagered,, Endagered, vulnerable, LeastConcern
+    created_at: u64,
+    updated_at: Option<u64>,
 }
-
-
 
 // Implement Storable and BoundedStorable traits for Taxonomy
 impl Storable for Taxonomy {
@@ -86,86 +83,84 @@ thread_local! {
     static TAXONOMY_STR: RefCell<StableBTreeMap<u64, Taxonomy, Memory>> =
         RefCell::new(StableBTreeMap::init(
             MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(2)))
-    ));
+        ));
 
     static MARINESPECIE_STR: RefCell<StableBTreeMap<u64, MarineSpecie, Memory>> =
         RefCell::new(StableBTreeMap::init(
             MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(3)))
-    ));
-
+        ));
 }
 
-#[derive(candid::CandidType, Serialize, Deserialize, Default,Validate)]
-struct TaxonomyPayload {
+#[derive(candid::CandidType, Serialize, Deserialize, Default, Validate)]
+struct TaxonomyInput {
     #[validate(length(min = 1))]
-    kingdom:String,
+    kingdom: String,
     #[validate(length(min = 1))]
-    phylum:String,
+    phylum: String,
     #[validate(length(min = 1))]
-    class:String,
+    class: String,
     #[validate(length(min = 1))]
-    order:String,
+    order: String,
     #[validate(length(min = 1))]
-    family:String,
+    family: String,
     #[validate(length(min = 1))]
-    genus:String,
+    genus: String,
     #[validate(length(min = 1))]
-    species:String,
+    species: String,
 }
 
-#[derive(candid::CandidType, Serialize, Deserialize, Default,Validate)]
-struct MarineSpeciePayload {
+#[derive(candid::CandidType, Serialize, Deserialize, Default, Validate)]
+struct MarineSpecieInput {
     #[validate(length(min = 1))]
     name: String,
     #[validate(length(min = 1))]
-    habitat:String,
+    habitat: String,
     #[validate(range(min = 0))]
-    taxonomy_id: u64, //Reference to Taxonomy by ID
+    taxonomy_id: u64, // Reference to Taxonomy by ID
     #[validate(length(min = 1))]
-    conservation_status:String,
+    conservation_status: String,
 }
 
-// CRUD Implementation below 
+// CRUD Implementation below
 #[ic_cdk::query]
 fn get_all_taxonomy() -> Result<Vec<Taxonomy>, Error> {
     let taxonomy_map: Vec<(u64, Taxonomy)> =
         TAXONOMY_STR.with(|service| service.borrow().iter().collect());
-    let taxonomys: Vec<Taxonomy> = taxonomy_map
+    let taxonomies: Vec<Taxonomy> = taxonomy_map
         .into_iter()
         .map(|(_, taxonomy)| taxonomy)
         .collect();
 
-    if !taxonomys.is_empty() {
-        Ok(taxonomys)
+    if !taxonomies.is_empty() {
+        Ok(taxonomies)
     } else {
         Err(Error::NotFound {
-            msg: "No Taxonomys found..... Empty! .".to_string(),
+            msg: "No Taxonomies found. Empty!".to_string(),
         })
     }
 }
+
 #[ic_cdk::query]
 fn get_taxonomy(id: u64) -> Result<Taxonomy, Error> {
     match _get_taxonomy(&id) {
         Some(taxonomy) => Ok(taxonomy),
         None => Err(Error::NotFound {
-            msg: format!("taxonomy with id={} not found", id),
+            msg: format!("Taxonomy with id={} not found", id),
         }),
     }
 }
 
-
-
-// Helper function 
+// Helper function
 fn _get_taxonomy(id: &u64) -> Option<Taxonomy> {
     TAXONOMY_STR.with(|service| service.borrow().get(id))
 }
 
 #[ic_cdk::update]
-fn add_taxonomy(taxonomy_payload: TaxonomyPayload) -> Result<Taxonomy, Error> {
-    let check_payload = taxonomy_payload.validate();
-    if check_payload.is_err() {
+fn add_taxonomy(taxonomy_input: TaxonomyInput) -> Result<Taxonomy, Error> {
+    let check_input = taxonomy_input.validate();
+    if check_input.is_err() {
         return Err(Error::ValidationFailed {
-            content: check_payload.err().unwrap().to_string(),
+            content: check_input.err().unwrap().to_string(),
         });
     }
     let id = TAXONOMY_ID_COUNTER
@@ -173,49 +168,46 @@ fn add_taxonomy(taxonomy_payload: TaxonomyPayload) -> Result<Taxonomy, Error> {
             let current_value = *counter.borrow().get();
             counter.borrow_mut().set(current_value + 1)
         })
-        .expect("cannot increment id counter");
-    let taxonomy = Taxonomy { 
+        .expect("Cannot increment id counter");
+    let taxonomy = Taxonomy {
         id,
-         kingdom: taxonomy_payload.kingdom,
-         phylum: taxonomy_payload.phylum,
-         class: taxonomy_payload.class,
-         order: taxonomy_payload.order,
-         family: taxonomy_payload.family,
-         genus: taxonomy_payload.genus,
-         species: taxonomy_payload.species,
-        created_at: time(), 
-        updated_at: None 
-    } ;
+        kingdom: taxonomy_input.kingdom,
+        phylum: taxonomy_input.phylum,
+        class: taxonomy_input.class,
+        order: taxonomy_input.order,
+        family: taxonomy_input.family,
+        genus: taxonomy_input.genus,
+        species: taxonomy_input.species,
+        created_at: time(),
+        updated_at: None,
+    };
     do_insert_taxonomy(&taxonomy);
     Ok(taxonomy)
 }
 
 #[ic_cdk::update]
-fn update_taxonomy(id: u64, taxonomy_payload: TaxonomyPayload) -> Result<Taxonomy, Error> {
-    let check_payload = taxonomy_payload.validate();
-    if check_payload.is_err() {
+fn update_taxonomy(id: u64, taxonomy_input: TaxonomyInput) -> Result<Taxonomy, Error> {
+    let check_input = taxonomy_input.validate();
+    if check_input.is_err() {
         return Err(Error::ValidationFailed {
-            content: check_payload.err().unwrap().to_string(),
+            content: check_input.err().unwrap().to_string(),
         });
     }
     match TAXONOMY_STR.with(|service| service.borrow().get(&id)) {
         Some(mut taxonomy) => {
-            taxonomy.kingdom = taxonomy_payload.kingdom;
-            taxonomy.phylum =   taxonomy_payload.phylum;
-            taxonomy.class =    taxonomy_payload.class;
-            taxonomy.order =    taxonomy_payload.order;
-            taxonomy.family =   taxonomy_payload.family;
-            taxonomy.genus =    taxonomy_payload.genus;
-            taxonomy.species =  taxonomy_payload.species;
+            taxonomy.kingdom = taxonomy_input.kingdom;
+            taxonomy.phylum = taxonomy_input.phylum;
+            taxonomy.class = taxonomy_input.class;
+            taxonomy.order = taxonomy_input.order;
+            taxonomy.family = taxonomy_input.family;
+            taxonomy.genus = taxonomy_input.genus;
+            taxonomy.species = taxonomy_input.species;
             taxonomy.updated_at = Some(time());
             do_insert_taxonomy(&taxonomy);
             Ok(taxonomy)
         }
         None => Err(Error::NotFound {
-            msg: format!(
-                "Update Taxonomy  with id={}. not found",
-                id
-            ),
+            msg: format!("Update Taxonomy with id={} not found", id),
         }),
     }
 }
@@ -235,21 +227,23 @@ fn delete_taxonomy(id: u64) -> Result<Taxonomy, Error> {
         Some(taxonomy) => Ok(taxonomy),
         None => Err(Error::NotFound {
             msg: format!(
-                "couldn't delete a taxonomy with id={}. taxonomy not found.",
+                "Couldn't delete a taxonomy with id={}. Taxonomy not found.",
                 id
             ),
         }),
     }
 }
 
-
-// Marine Specie 
+// Marine Specie
 
 #[ic_cdk::query]
 fn get_all_marinespecie() -> Result<Vec<MarineSpecie>, Error> {
     let marinespecie_map: Vec<(u64, MarineSpecie)> =
         MARINESPECIE_STR.with(|service| service.borrow().iter().collect());
-    let marinespecies: Vec<MarineSpecie> = marinespecie_map.into_iter().map(|(_, marinespecie)| marinespecie).collect();
+    let marinespecies: Vec<MarineSpecie> = marinespecie_map
+        .into_iter()
+        .map(|(_, marinespecie)| marinespecie)
+        .collect();
 
     if !marinespecies.is_empty() {
         Ok(marinespecies)
@@ -260,7 +254,6 @@ fn get_all_marinespecie() -> Result<Vec<MarineSpecie>, Error> {
     }
 }
 
-
 #[ic_cdk::query]
 fn get_marinespecie(id: u64) -> Result<MarineSpecie, Error> {
     match _get_marinespecie(&id) {
@@ -270,12 +263,11 @@ fn get_marinespecie(id: u64) -> Result<MarineSpecie, Error> {
         }),
     }
 }
+
 // a helper method to get a marine specie by id.
 fn _get_marinespecie(id: &u64) -> Option<MarineSpecie> {
     MARINESPECIE_STR.with(|service| service.borrow().get(id))
 }
-
-
 
 // Get marine specie by conservation_status
 #[ic_cdk::query]
@@ -294,18 +286,20 @@ fn get_marinespecie_by_conservation_status(conservation_status: String) -> Resul
         Ok(marinespecie_in_conservation_status)
     } else {
         Err(Error::NotFound {
-            msg: format!("No Marine Specie  found in classified conservation_status: {}", conservation_status),
+            msg: format!(
+                "No Marine Specie found in classified conservation_status: {}",
+                conservation_status
+            ),
         })
     }
 }
 
-
 #[ic_cdk::update]
-fn add_marinespecie(marinespecie_payload: MarineSpeciePayload) -> Result<MarineSpecie, Error> {
-    let check_payload = marinespecie_payload.validate();
-    if check_payload.is_err() {
+fn add_marinespecie(marinespecie_input: MarineSpecieInput) -> Result<MarineSpecie, Error> {
+    let check_input = marinespecie_input.validate();
+    if check_input.is_err() {
         return Err(Error::ValidationFailed {
-            content: check_payload.err().unwrap().to_string(),
+            content: check_input.err().unwrap().to_string(),
         });
     }
     let id = MARINESPECIE_ID_COUNTER
@@ -313,41 +307,41 @@ fn add_marinespecie(marinespecie_payload: MarineSpeciePayload) -> Result<MarineS
             let current_value = *counter.borrow().get();
             counter.borrow_mut().set(current_value + 1)
         })
-        .expect("cannot increment id counter");
+        .expect("Cannot increment id counter");
 
-    let marinespecie = MarineSpecie { 
+    let marinespecie = MarineSpecie {
         id,
-        name: marinespecie_payload.name,
-        habitat: marinespecie_payload.habitat,
-        taxonomy_id: marinespecie_payload.taxonomy_id,
-        conservation_status: marinespecie_payload.conservation_status,
+        name: marinespecie_input.name,
+        habitat: marinespecie_input.habitat,
+        taxonomy_id: marinespecie_input.taxonomy_id,
+        conservation_status: marinespecie_input.conservation_status,
         created_at: time(),
-        updated_at: None
+        updated_at: None,
     };
     do_insert_marinespecie(&marinespecie);
     Ok(marinespecie)
 }
 
 #[ic_cdk::update]
-fn update_marinespecie(id: u64, marinespecie_payload: MarineSpeciePayload) -> Result<MarineSpecie, Error> {
-    let check_payload = marinespecie_payload.validate();
-    if check_payload.is_err() {
+fn update_marinespecie(id: u64, marinespecie_input: MarineSpecieInput) -> Result<MarineSpecie, Error> {
+    let check_input = marinespecie_input.validate();
+    if check_input.is_err() {
         return Err(Error::ValidationFailed {
-            content: check_payload.err().unwrap().to_string(),
+            content: check_input.err().unwrap().to_string(),
         });
     }
     match MARINESPECIE_STR.with(|service| service.borrow().get(&id)) {
         Some(mut marinespecie) => {
-            marinespecie.name = marinespecie_payload.name;
-            marinespecie.habitat = marinespecie_payload.habitat;
-            marinespecie.taxonomy_id = marinespecie_payload.taxonomy_id;
-            marinespecie.conservation_status = marinespecie_payload.conservation_status;
+            marinespecie.name = marinespecie_input.name;
+            marinespecie.habitat = marinespecie_input.habitat;
+            marinespecie.taxonomy_id = marinespecie_input.taxonomy_id;
+            marinespecie.conservation_status = marinespecie_input.conservation_status;
             marinespecie.updated_at = Some(time());
             do_insert_marinespecie(&marinespecie);
             Ok(marinespecie)
         }
         None => Err(Error::NotFound {
-            msg: format!("could not update Marine Specie with id={}.", id),
+            msg: format!("Could not update Marine Specie with id={}.", id),
         }),
     }
 }
@@ -356,23 +350,25 @@ fn update_marinespecie(id: u64, marinespecie_payload: MarineSpeciePayload) -> Re
 fn do_insert_marinespecie(marinespecie: &MarineSpecie) {
     MARINESPECIE_STR.with(|service| service.borrow_mut().insert(marinespecie.id, marinespecie.clone()));
 }
+
 #[ic_cdk::update]
 fn delete_marinespecie(id: u64) -> Result<MarineSpecie, Error> {
     match MARINESPECIE_STR.with(|service| service.borrow_mut().remove(&id)) {
         Some(marinespecie) => Ok(marinespecie),
         None => Err(Error::NotFound {
             msg: format!(
-                "couldn't delete a marinespecie with id={}.",
+                "Couldn't delete a marinespecie with id={}.",
                 id
             ),
         }),
     }
 }
+
 #[derive(candid::CandidType, Deserialize, Serialize)]
 enum Error {
     NotFound { msg: String },
     ValidationFailed { content: String },
-    InvalidInput 
+    InvalidInput,
 }
 
 // need this to generate candid
